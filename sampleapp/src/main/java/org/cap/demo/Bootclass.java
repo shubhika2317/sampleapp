cc02e533d5313985aee55f017899a833894e4009
shubhika2317 <shubhika2317@gmail.com>
2021-09-17 17:58:07 +0530

    first commit

---
+++ sampleapp/src/main/java/org/cap/demo/Bootclass.java
@@ -0,0 +1,10 @@
+package org.cap.demo;
+
+public class Bootclass {
+
+	public static void main(String[] args) {
+		System.out.println("hello! from sts!");// TODO Auto-generated method stub
+
+	}
+
+}
